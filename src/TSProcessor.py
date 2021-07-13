4d910e3c5dfe8d05f71e282612456241d0cedd68
thxi <thxixl@protonmail.com>
2021-07-13 17:00:31 +0300

    gpu/cpu implementation

--- src/TSProcessor.py
+++ src/TSProcessor.py
@@ -1,15 +1,21 @@
-import time
+from typing import Union
 
 import numpy as np
 import cupy as cp
-from joblib import Parallel, delayed
 from sklearn.cluster import DBSCAN
+from cuml import DBSCAN as cumlDBSCAN
+from joblib import Parallel, delayed
+
+# https://stackoverflow.com/a/55239060
+from timeit import default_timer as timer
+from datetime import timedelta
 
 # TODO: fix docstrings
 
 
 class TSProcessor:
-    def __init__(self, *, points_in_template: int, max_template_spread: int):
+    def __init__(self, points_in_template: int, max_template_spread: int,
+                 X_train: Union[np.ndarray, cp.ndarray]):
 
         # максимальное расстояние между соседними зубчиками шаблона
         self._max_template_spread = max_template_spread
@@ -19,12 +25,13 @@ class TSProcessor:
             points_in_template - 1)  # сколько у нас всего шаблонов
         self.z_dim: int = points_in_template  # сколько зубчиков в каждом шаблоне
 
-        # сами шабл)оны
-        templates = (cp.zeros(shape=(self.x_dim, 1), dtype=int), )
+        xp = cp.get_array_module(X_train)
+        # сами шаблоны
+        templates = (xp.zeros(shape=(self.x_dim, 1), dtype=int), )
         # код, который заполняет шаблоны нужными значениями
         for i in range(1, points_in_template):
             col = (
-                cp.repeat(cp.arange(1, max_template_spread + 1, dtype=int),
+                xp.repeat(xp.arange(1, max_template_spread + 1, dtype=int),
                           max_template_spread**(points_in_template -
                                                 (i + 1))) +
                 templates[i - 1][::max_template_spread**(points_in_template -
@@ -32,17 +39,20 @@ class TSProcessor:
 
             templates += (col, )
 
-        self._templates: cp.ndarray = cp.hstack(templates)
+        self._templates: xp.ndarray = xp.hstack(templates)
 
         # формы шаблонов, т.е. [1, 1, 1], [1, 1, 2] и т.д.
-        self._template_shapes: cp.ndarray = self._templates[:,1:] \
+        self._template_shapes: xp.ndarray = self._templates[:,1:] \
             - self._templates[:, :-1]  # k1, k2, ...
 
-    def fit(self, time_series: cp.ndarray) -> None:
+        self.__fit(X_train)
+
+    def __fit(self, X_train: Union[np.ndarray, cp.ndarray]) -> None:
         """ Fill training vectors from time_series """
 
-        self.y_dim = time_series.shape[0] - int(
-            cp.asnumpy(self._templates[0][-1]))
+        xp = cp.get_array_module(X_train)
+
+        self.y_dim = X_train.shape[0] - int(self._templates[0][-1])
 
         # создать обучающее множество
         # Его можно представить как куб, где по оси X идут шаблоны, по оси Y - вектора,
@@ -50,19 +60,22 @@ class TSProcessor:
         # Чтобы получить точку A вектора B шаблона C - делаем self._training_vectors[C, B, A].
         # Вектора идут в хронологическом порядке "протаскивания" конкретного шаблона по ряду,
         # шаблоны - по порядку от [1, 1, ... , 1], [1, 1, ..., 2] до [n, n, ..., n].
-        self._training_vectors: cp.ndarray = \
-            cp.full(shape=(self.x_dim, self.y_dim, self.z_dim), fill_value=cp.inf, dtype=float)
+        self._training_vectors = xp.full(shape=(self.x_dim, self.y_dim,
+                                                self.z_dim),
+                                         fill_value=xp.inf,
+                                         dtype=float)
 
         # тащим шаблон по ряду
         for i in range(self.x_dim):
-            template_data = (time_series[
+            template_data = (X_train[
                 self._templates[i] +
-                cp.arange(time_series.size - self._templates[i][-1])[:, None]])
+                xp.arange(X_train.size - self._templates[i][-1])[:, None]])
 
-            self._training_vectors[i, :template_data.shape[0]] = (time_series[
+            self._training_vectors[i, :template_data.shape[0]] = (X_train[
                 self._templates[i] +
-                cp.arange(time_series.size - self._templates[i][-1])[:, None]])
-        self._last_vectors = cp.cumsum(-self._template_shapes[:, ::-1],
+                xp.arange(X_train.size - self._templates[i][-1])[:, None]])
+
+        self._last_vectors = xp.cumsum(-self._template_shapes[:, ::-1],
                                        axis=1)[:, ::-1]
 
     def heal(
@@ -70,6 +83,167 @@ class TSProcessor:
         X_pred,
     ):
         raise NotImplementedError()
+
+    def predict_trajectories(
+        self,
+        X_start: Union[cp.ndarray, np.ndarray],  # forecast after X_start
+        h_max: int,  # forecasting horizon
+        eps: float,  # eps for distance matrix
+        n_trajectories: int,
+        noise_amp: float,
+        use_priori=False,
+        X_test: Union[cp.ndarray, np.ndarray] = None,
+        priori_eps=0.1,
+        random_seed=1,
+        n_jobs: int = -1,
+        print_time=False,
+    ) -> np.ndarray:
+        """
+        Get trajectories' predictions from X_start to X_test
+
+        :param steps: На сколько шагов прогнозируем.
+        :param eps: Минимальное Евклидово расстояние от соответствующего шаблона, в пределах которого должны находиться
+            вектора наблюдений, чтобы считаться "достаточно похожими".
+        :param n_trajectories: Сколько траекторий использовать.
+            Чем больше, тем дольше время работы и потенциально точнее результат.
+        :param noise_amp: Максимальная амплитуда шума, используемая при расчете траекторий.
+        :param n_jobs: максимальное количество процессов для вычисления каждой траектории
+        :param prev_result: предыдущие предсказания
+        :return: Возвращает матрицу размером steps x n_trajectories, где по горизонтали идут шаги, а по вертикали - прогнозы
+        каждой из траекторий на этом шаге.
+        """
+        xp = cp.get_array_module(X_start)
+        if xp.__name__ == 'numpy':
+            return self._predict_trajectories_cpu(X_start, h_max, eps,
+                                                  n_trajectories, noise_amp,
+                                                  use_priori, X_test,
+                                                  priori_eps, random_seed,
+                                                  n_jobs, print_time)
+        return self._predict_trajectories_gpu(X_start, h_max, eps,
+                                              n_trajectories, noise_amp,
+                                              use_priori, X_test, priori_eps)
+
+    def _predict_trajectories_gpu(
+        self,
+        X_start: cp.ndarray,  # forecast after X_start
+        h_max: int,  # forecasting horizon
+        eps: float,  # eps for distance matrix
+        n_trajectories: int,
+        noise_amp: float,
+        use_priori=False,
+        X_test: cp.ndarray = None,
+        priori_eps=0.1,
+    ) -> cp.ndarray:
+
+        assert (X_start.shape[0] == self._max_template_spread *
+                (self._points_in_template - 1)), "X_start should be bigger"
+
+        if use_priori:
+            assert X_test is not None, 'X_test should be specified'
+
+        # doign this to fill X_start
+        original_size = X_start.shape[0]
+        X_start = cp.resize(X_start, X_start.shape[0] + h_max)
+        X_start[-h_max:] = cp.nan
+
+        X_start = cp.repeat(X_start[cp.newaxis, :], n_trajectories, axis=0)
+        training_for_dist = cp.repeat(self._training_vectors[cp.newaxis, :, :,
+                                                             -1],
+                                      n_trajectories,
+                                      axis=0)
+        noise = cp.random.normal(0, noise_amp, size=(n_trajectories, h_max))
+        for i in range(h_max):
+            test_vectors = X_start[:, :original_size + i][:,
+                                                          self._last_vectors]
+            dist = _calc_distance_matrix_gpu(self._training_vectors,
+                                             test_vectors)
+
+            # see https://stackoverflow.com/a/29046530
+            predictions = cp.nanmean(cp.where(dist < eps, training_for_dist,
+                                              cp.nan).reshape(
+                                                  n_trajectories, -1),
+                                     axis=1) + noise[:, i]
+            if use_priori:
+                predictions = cp.where(
+                    cp.abs(predictions - X_test[i]) < priori_eps, predictions,
+                    cp.nan)
+            X_start[:, original_size + i] = predictions
+
+        # сеты прогнозных значений для каждой точки, в которой будем делать прогноз
+        # размер: steps x n_trajectories
+        X_start = X_start[:, original_size:]
+        X_start = X_start.T
+        return X_start
+
+    def _predict_trajectories_cpu(
+        self,
+        X_start: np.ndarray,  # forecast after X_start
+        h_max: int,  # forecasting horizon
+        eps: float,  # eps for distance matrix
+        n_trajectories: int,
+        noise_amp: float,
+        use_priori=False,
+        X_test: np.ndarray = None,
+        priori_eps=0.1,
+        random_seed=1,
+        n_jobs: int = -1,
+        print_time=False,
+    ) -> np.ndarray:
+        assert (X_start.shape[0] == self._max_template_spread *
+                (self._points_in_template - 1)), "X_start should be bigger"
+
+        if use_priori:
+            assert X_test is not None, 'X_test should be specified'
+
+        # doign this to fill X_start
+        original_size = X_start.shape[0]
+        X_start = np.resize(X_start, X_start.shape[0] + h_max)
+        X_start[-h_max:] = np.nan
+
+        def get_trajectory_forecast(
+            i: int,
+            X_start: np.ndarray,
+        ) -> np.ndarray:
+            np.random.seed(random_seed * i)
+            X_start = X_start.copy()
+            forecast_set = np.full((h_max, ), np.nan)
+            for j in range(h_max):
+                # тестовые вектора, которые будем сравнивать с тренировочными
+                test_vectors = X_start[:original_size + j][self._last_vectors]
+
+                distance_matrix = _calc_distance_matrix_cpu(
+                    self._training_vectors, test_vectors)
+
+                # последние точки тренировочных векторов, оказавшихся в пределах eps
+                points = self._training_vectors[distance_matrix < eps][:, -1]
+                if np.all(points == np.nan):
+                    continue
+
+                # теперь нужно выбрать финальное прогнозное значение из возможных
+                forecast_point = points.mean() + np.random.normal(0, noise_amp)
+
+                if use_priori:
+                    if np.abs(forecast_point - X_test[j]) < priori_eps:
+                        forecast_set[j] = forecast_point
+                        X_start[original_size + j] = forecast_point
+                else:
+                    forecast_set[j] = forecast_point
+                    X_start[original_size + j] = forecast_point
+            return forecast_set
+
+        start = timer()
+        X_traj_pred = Parallel(n_jobs=n_jobs, backend="threading")(
+            delayed(get_trajectory_forecast)(i, X_start)
+            for i in range(n_trajectories))
+        end = timer()
+        if print_time:
+            print(timedelta(seconds=end - start))
+
+        # сеты прогнозных значений для каждой точки, в которой будем делать прогноз
+        # размер: steps x n_trajectories
+        X_traj_pred = np.array(X_traj_pred).T
+
+        return X_traj_pred
 
     def predict_unified(
         self,
@@ -98,12 +272,12 @@ class TSProcessor:
             assert priori_eps is not None
 
         if method == 'cluster':
-            traj_alive, cluster_centers, X_pred = self.cluster_sets(
+            traj_alive, cluster_centers, X_pred = self._cluster_sets(
                 X_traj_pred, dbs_min_trajectories, dbs_eps, dbs_min_samples)
             res['traj_alive'] = traj_alive
             res['cluster_centers'] = cluster_centers
         elif method == 'quantile':
-            qs, traj_alive, X_pred = self.get_quantile_prediction(
+            qs, traj_alive, X_pred = self._get_quantile_prediction(
                 X_traj_pred, min_trajectories, max_err, alpha)
             res['qs'] = qs
             res['traj_alive'] = traj_alive
@@ -117,79 +291,13 @@ class TSProcessor:
 
         return res
 
-    def predict_trajectories(
+    def _cluster_sets(
         self,
-        X_start: cp.ndarray,  # forecast after X_start
-        h_max: int,  # forecasting horizon
-        eps: float,  # eps for distance matrix
-        n_trajectories: int,
-        noise_amp: float,
-        use_priori=False,
-        X_test: cp.ndarray = None,
-        priori_eps=0.1,
-        save_first_type_non_pred=False,  # save distance matrix
-        random_seed=1,
-        n_jobs: int = -1,
-        print_time=True,
-    ) -> np.ndarray:
-        """
-        Get trajectories' predictions from X_start to X_test
-
-        :param steps: На сколько шагов прогнозируем.
-        :param eps: Минимальное Евклидово расстояние от соответствующего шаблона, в пределах которого должны находиться
-            вектора наблюдений, чтобы считаться "достаточно похожими".
-        :param n_trajectories: Сколько траекторий использовать.
-            Чем больше, тем дольше время работы и потенциально точнее результат.
-        :param noise_amp: Максимальная амплитуда шума, используемая при расчете траекторий.
-        :param n_jobs: максимальное количество процессов для вычисления каждой траектории
-        :param prev_result: предыдущие предсказания
-        :return: Возвращает матрицу размером steps x n_trajectories, где по горизонтали идут шаги, а по вертикали - прогнозы
-        каждой из траекторий на этом шаге.
-        """
-
-        assert (X_start.shape[0] >= self._max_template_spread *
-                (self._points_in_template - 1)), "X_start should be bigger"
-
-        if use_priori:
-            assert X_test is not None, 'X_test should be specified'
-
-        # doign this to fill X_start
-        original_size = X_start.shape[0]
-        X_start = cp.resize(X_start, X_start.shape[0] + h_max)
-        X_start[-h_max:] = cp.nan
-
-        X_start = cp.repeat(X_start[cp.newaxis, :], n_trajectories, axis=0)
-        training_for_dist = cp.repeat(self._training_vectors[cp.newaxis, :, :,
-                                                             -1],
-                                      n_trajectories,
-                                      axis=0)
-        noise = cp.random.normal(0, noise_amp, size=(n_trajectories, h_max))
-        for j in range(h_max):
-            last_vectors = (X_start[:, :original_size + j][:,
-                                                           self._last_vectors])
-            dist = _calc_distance_matrix(self._training_vectors, last_vectors)
-
-            # see https://stackoverflow.com/a/29046530
-            predictions = cp.nanmean(cp.where(dist < eps, training_for_dist,
-                                              cp.nan).reshape(
-                                                  n_trajectories, -1),
-                                     axis=1) + noise[:, j]
-            X_start[:, original_size + j] = predictions
-
-        # сеты прогнозных значений для каждой точки, в которой будем делать прогноз
-        # размер: steps x n_trajectories
-        X_start = X_start[:, original_size:]
-        X_start = X_start.T
-        # X_start = cp.asnumpy(X_start)
-        return X_start
-
-    def cluster_sets(
-        self,
-        X_traj_pred: np.ndarray,
+        X_traj_pred: Union[np.ndarray, cp.ndarray],
         min_trajectories,
         dbs_eps: float,
         dbs_min_samples: int,
-    ):
+    ) -> Union[np.ndarray, cp.ndarray]:
         """
         Скластеризировать полученные в результате пулла множества прогнозных значений.
 
@@ -199,10 +307,15 @@ class TSProcessor:
         :return: Возвращает центр самого большого кластера на каждом шаге.
         """
 
-        X_pred = np.full(shape=[
+        xp = cp.get_array_module(X_traj_pred)
+
+        X_pred = xp.full(shape=[
             X_traj_pred.shape[0],
-        ], fill_value=np.nan)
-        dbs = DBSCAN(eps=dbs_eps, min_samples=dbs_min_samples)
+        ], fill_value=xp.nan)
+        if xp.__name__ == 'numpy':
+            dbs = DBSCAN(eps=dbs_eps, min_samples=dbs_min_samples)
+        else:
+            dbs = cumlDBSCAN(eps=dbs_eps, min_samples=dbs_min_samples)
 
         cluster_centers = []
         traj_alive = []
@@ -217,7 +330,9 @@ class TSProcessor:
             dbs.fit(traj_pred.reshape(-1, 1))
 
             # TODO: handle case when there are 2 big clusters
-            cluster_labels, cluster_sizes = np.unique(
+            if xp.all(dbs.labels_ == -1):
+                continue
+            cluster_labels, cluster_sizes = xp.unique(
                 dbs.labels_[dbs.labels_ > -1], return_counts=True)
             # print(i, cluster_sizes)
 
@@ -238,13 +353,13 @@ class TSProcessor:
 
         return traj_alive, cluster_centers, X_pred
 
-    def get_quantile_prediction(
+    def _get_quantile_prediction(
         self,
-        X_traj_pred: np.ndarray,
+        X_traj_pred: cp.ndarray,
         min_trajectories,
         max_err: float = 0.1,
         alpha=0.3,
-    ):
+    ) -> Union[np.ndarray, cp.ndarray]:
         """
         Скластеризировать полученные в результате пулла множества прогнозных значений.
 
@@ -254,9 +369,14 @@ class TSProcessor:
         :return: Возвращает среднее предсказание в [alpha, 1-alpha] квантилях
         """
 
+        xp = cp.get_array_module(X_traj_pred)
+
+        if xp.__name__ != 'numpy':
+            X_traj_pred = cp.asnumpy(X_traj_pred)
+
         X_pred = np.full(shape=[
             X_traj_pred.shape[0],
-        ], fill_value=cp.nan)
+        ], fill_value=np.nan)
 
         qs = []
         traj_alive = []
@@ -280,10 +400,13 @@ class TSProcessor:
 
         qs = np.array(qs)
         traj_alive = np.array(traj_alive)
+
+        if xp.__name__ != 'numpy':
+            X_traj_pred = cp.asarray(X_traj_pred)
         return qs, traj_alive, X_pred
 
 
-def _calc_distance_matrix(
+def _calc_distance_matrix_gpu(
     training_vectors: cp.ndarray,
     test_vectors: cp.ndarray,
 ) -> cp.ndarray:
@@ -299,5 +422,24 @@ def _calc_distance_matrix(
     test_vectors = test_vectors[:, :, cp.newaxis, :]
 
     distances = cp.sqrt(((training_vectors - test_vectors)**2).sum(axis=-1))
+
+    return distances
+
+
+def _calc_distance_matrix_cpu(
+    training_vectors: np.ndarray,
+    test_vectors: np.ndarray,
+) -> np.ndarray:
+    """
+    calculate the distance matrix between training_vectors and test_vectors
+    """
+
+    # drop last point from training vectors
+    training_vectors = training_vectors[:, :, :-1]
+
+    # reshaping test_vectors to efficiently calculate the distances
+    test_vectors = test_vectors[:, cp.newaxis, :]
+
+    distances = np.sqrt(((training_vectors - test_vectors)**2).sum(axis=-1))
 
     return distances
